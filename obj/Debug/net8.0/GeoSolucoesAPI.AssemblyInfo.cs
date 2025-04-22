927607ee824448062475728e3a087d796736e619
Matheus Rocha <im.rochamatheus@gmail.com>
2025-04-22 14:29:46 -0300

    :wrench: environment: configurar conexão com banco de dados remoto

--- obj/Debug/net8.0/GeoSolucoesAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/GeoSolucoesAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GeoSolucoesAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+961391f88ec6ad4aeb54be3cd993fd9d155e2900")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94c6c016e8efdcffd60b6c9bbe9e5cbf85ee3929")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeoSolucoesAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GeoSolucoesAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
